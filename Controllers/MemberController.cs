df37e7838217b8eec81318aa241e82d4127464bc
Francesca Stocco <francescaj.stocco@gmail.com>
2021-07-06 13:36:53 +0100

    created controller, view and model

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,19 @@
+﻿using Bookish.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookish.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            var membersList = new MembersListViewModel();
+            //membersList.Name = "Ken";
+            return View(membersList);
+        }
+    }
+}
---
+++ Models/MembersListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookish.Models
+{
+    public class MembersListViewModel
+    {
+        public int[] Id = { 1, 2, 3, 4 };
+        public string[] Name = { "ken", "francesca", "oskar", "luke"};
+    }
+}
